1ec9b92b72996cea40dee0da0698a150eee19f06
Pedro <pedrosantoscrlx@gmail.com>
2025-11-10 22:02:25 +0000

    Add user authentication forms, update URLs, and create HTML templates for admin dashboard and index page

--- django-app/library/urls.py
+++ django-app/library/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import admin_login
+from .views import login_admin, logout_admin, index
 
 urlpatterns = [
-    path("library/login/", admin_login, name="admin_login"),
+    path("login_admin/", login_admin, name="admin_login"),
+    path("logout_admin/", logout_admin, name="logout_admin"),
+    path("", index, name="index"),
 ]
