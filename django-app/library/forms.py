564cf65af1258c93aa36d3fbded57d2ba07f39ec
Pedrocrlx <100964693+Pedrocrlx@users.noreply.github.com>
2025-11-11 20:43:28 +0000

    Revert "Feat/list books"

--- django-app/library/forms.py
+++ django-app/library/forms.py
@@ -3,4 +3,3 @@ from django import forms
 class AdminLoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
-
--- django-app/library/urls.py
+++ django-app/library/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import admin_login, book_list
+from .views import admin_login
 
 urlpatterns = [
     path("library/login/", admin_login, name="admin_login"),
-    path("library/books/", book_list , name="book_list"),  
 ]
-
